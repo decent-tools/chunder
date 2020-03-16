cf7535d4dd5564088785b8d088067c1283403a42
Barath Kumar <barath.kumar@buynomics.com>
2020-03-16 22:28:03 +0100

    feat: styles

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 const App = () => {
-  return <h1>Hello World!!</h1>
+  return <h1>Hello World</h1>
 }
 
 export default hot(App)
